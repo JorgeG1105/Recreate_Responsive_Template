d7161765869c2d41ac4d199c143310bb12b8ccde
JorgeG1105 <J_goris@live.com>
2018-08-16 09:09:01 +0100

    Woops! corrected a mistake with the toggle function on the mobile navigation bar.

--- script.js
+++ script.js
@@ -3,10 +3,5 @@ let mainNav = document.querySelector(".mainNav")
 
 hamburger.addEventListener("click", function(){
 	this.classList.toggle("change");
-	if (mainNav.style.display === "block") {
-		mainNav.style.display = "none";
-	}
-	else {
-		mainNav.style.display = "block";
-	}
+	mainNav.classList.toggle("toggleDisplay");
 });
